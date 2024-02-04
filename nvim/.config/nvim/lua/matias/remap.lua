8e408d83fa0ab5128a80189b1d5c3e4ba4406514
MatiMoran <https://github.com/MatiMoran>
2024-02-04 17:34:56 -0300

    nvim config

--- nvim/.config/nvim/lua/matias/remap.lua
+++ nvim/.config/nvim/lua/matias/remap.lua
@@ -1,3 +1,5 @@
+vim.g.mapleader = " "
+
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
@@ -8,3 +10,17 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
+
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+vim.keymap.set("n", "<leader>.", vim.lsp.buf.code_action)
+
+vim.keymap.set("n", "<leader><leader>", function()
+    vim.cmd("so")
+end)
