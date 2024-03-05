6fcfcdaa7b3832cc6621590ff4a3ef5ee84ee532
MatiMoran <https://github.com/MatiMoran>
2024-03-04 21:13:44 -0300

    add lsp

--- nvim/.config/nvim/lua/matias/plugins/init.lua
+++ nvim/.config/nvim/lua/matias/plugins/init.lua
@@ -11,15 +11,15 @@ return {
       'Mofiqul/vscode.nvim',
       'ThePrimeagen/harpoon',
       'mbbill/undotree',
-      'tpope/vim-fugitive'
- --     'williamboman/mason.nvim',
- --     'williamboman/mason-lspconfig.nvim',
- --     'neovim/nvim-lspconfig',
- --     'hrsh7th/nvim-cmp',
- --     'hrsh7th/cmp-nvim-lsp',
- --     'L3MON4D3/LuaSnip',
- --     {
- --         'VonHeikemen/lsp-zero.nvim',
- --         branch = 'v3.x',
- --     }
+      'tpope/vim-fugitive',
+      'williamboman/mason.nvim',
+      'williamboman/mason-lspconfig.nvim',
+      {
+          'VonHeikemen/lsp-zero.nvim',
+          branch = 'v3.x',
+      },
+      'neovim/nvim-lspconfig',
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/nvim-cmp',
+      'L3MON4D3/LuaSnip'
 }
