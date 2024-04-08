79862015aec31c4872f740fd28d333c4f62dd79b
MatiasMoran <maatiasnicolas.moran@mercadolibre.com>
2024-04-08 16:00:59 -0300

    first macos configs

---
+++ macos/nvim/.config/nvim/lua/matias/core/options.lua
@@ -0,0 +1,34 @@
+local opt = vim.opt
+
+opt.number = true
+opt.relativenumber = true
+
+opt.tabstop = 4
+opt.softtabstop = 4
+opt.shiftwidth = 4
+opt.autoindent = true
+opt.expandtab = true
+
+opt.smartindent = true
+
+opt.wrap = false
+
+opt.swapfile = false
+opt.backup = false
+opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+opt.undofile = true
+
+opt.hlsearch = false
+opt.incsearch = true
+
+opt.termguicolors = true
+
+opt.scrolloff = 16
+
+opt.updatetime = 50
+
+
+
+-- disabled to nvim-tree
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
